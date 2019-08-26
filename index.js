62d99b679e22e08e2296935c18d2929223e177bc
chinatattoo <chinatattoo.contato@gmail.com>
2019-08-26 16:53:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const readline = require('readline-sync')
 
 function start(){
     const content = {}
-    content.searchTerm = askAndReturnSearchTerm() {
+    content.searchTerm = askAndReturnSearchTerm()
+
+
+        function askAndReturnSearchTerm(){
 
         return readline.question('Type a Wikipedia search term: ')
     }
